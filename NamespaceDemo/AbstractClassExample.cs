9c2523f5409addd1c973057784809165b73b424e
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-16 11:57:03 +0800

    v1.3

---
+++ NamespaceDemo/AbstractClassExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceDemo
+{
+
+    class parent { } // if you put sealed class on parent, Child class cannot inherit it
+    sealed class child : parent { } // if you put sealed class on child, below example class cannot inherit it
+
+    class example : child { }
+    class AbstractClassExample
+    {
+
+        public static void Main ()
+        {
+
+        }
+    }
+}
